5d003d699488c8276a61529d7d6a7eaa1d6d2aed
marty <marty@DESKTOP-641FKEE>
2024-12-19 21:35:52 +0100

    Dodaj pliki projektów.

---
+++ MyEnhancedApp/Controllers/ContactController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyEnhancedApp.Controllers
+{
+    public class ContactController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Submit(string name, string email, string message)
+        {
+            ViewBag.Name = name;
+            ViewBag.Email = email;
+            ViewBag.Message = message;
+            return View("ThankYou");
+        }
+    }
+}
---
+++ MyEnhancedApp/Controllers/WeatherController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyEnhancedApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeatherController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetWeather()
+        {
+            var weatherData = new[]
+            {
+                new { Date = "2024-12-19", TemperatureC = 5, Summary = "Cloudy" },
+                new { Date = "2024-12-20", TemperatureC = 3, Summary = "Rainy" },
+                new { Date = "2024-12-21", TemperatureC = -1, Summary = "Snowy" }
+            };
+
+            return Ok(weatherData);
+        }
+    }
+}
